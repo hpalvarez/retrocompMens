ea24c6729b12e748d2ef0fa3d94f929ccac866e6
Hernán Alvarez <hernan.alvarez@gmail.com>
2018-06-03 09:25:04 -0300

    Se actualiza documentación

--- retrocompMens.py
+++ retrocompMens.py
@@ -1,20 +1,35 @@
+# Scraper de mensajes de Retrocomputación
+
+# Importo BeautifulSoup 4 (requerido) y requests (trae la página), argparse (linea de comandos) y csv (para la salida)
+
 from bs4 import BeautifulSoup
 import requests, argparse, csv
 
+# Leo de la línea de comandos cuantos mensajes quiero, por default traigo 20
+
 parser = argparse.ArgumentParser(description='Trae mensajes del foro Retrocomputación.')
 parser.add_argument('mensajes', help = 'Cantidad de mensajes a traer (default:20)', type = int, nargs = '?', default = '20')
-parser.add_argument('csv', help = 'Cantidad de mensajes a traer (default:20)', type = int, nargs = '?', default = '20')
 argumentos = parser.parse_args()
 
-URL = 'http://www.retrocomputacion.com/e107_plugins/forum/forum_latest.php?' + str(argumentos.mensajes)
-pagina = requests.get(URL)
+# Armo la URL y la traigo
+
+url = 'http://www.retrocomputacion.com/e107_plugins/forum/forum_latest.php?' + str(argumentos.mensajes)
+pagina = requests.get(url)
+
+# Preparo la sopa
 
 sopa = BeautifulSoup(pagina.text,'html.parser')
+
+# Extraigo los mensajes y los autores de la tabla, usando los atributos de nombre y estilo de cada celda
 
 mensajes = sopa.find_all('td', attrs={'class': 'forumheader3', 'style': 'width:60%'})
 autores = sopa.find_all('td', attrs={'class': 'forumheader3', 'style': 'width:27%; text-align:center'})
 
+# Preparo la lista de salida
+
 salida = []
+
+# Extraigo los datos
 
 for mensaje, autor in zip(mensajes, autores):
     titulo = mensaje.contents[0].string
@@ -25,6 +40,8 @@ for mensaje, autor in zip(mensajes, autores):
     print("\nTITULO: " + titulo)
     print("USUARIO: " + user)
     print("CUERPO: " + cuerpo)
+
+# Usando la lista "salida" genero el CSV
 
 with open('mensajes.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['titulo', 'user', 'cuerpo']
